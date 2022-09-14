aa5d1c26c502e21b6744b0b886724d78f44f8c4f
pranshi <92673461+coderSHeE@users.noreply.github.com>
2022-09-14 22:00:08 +0530

    Update main.js

--- assigment2/main.js
+++ assigment2/main.js
@@ -1,7 +1,7 @@
-var modal = document.getElementById('id01');
+var modal = document.getElementById('id01');                // we can make only one Id with same name !   
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+
+window.onclick = function(event) {                          
   if (event.target == modal) {
     modal.style.display = "none";
   }
